6dbc0f10012e6ef9dfdb7ac0786b109263cbb18c
augustin-marie <a.marie189@laposte.net>
2023-02-17 15:50:34 +0100

    test fix

--- application/selDriver.py
+++ application/selDriver.py
@@ -1,27 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
-class Sel_driver:
-    def __init__ (self):
-        self.driver = webdriver.Chrome()
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(options=chrome_options)
 
-    #def get_roulette_title(self):
-    #    self.driver.get("https://wikiroulette.co/")
-    #    title_element = self.driver.find_element(By.CSS_SELECTOR, "#firstHeading span")
-    #    title = title_element.text
-    #    print(title)
-    #    return title
+#def get_roulette_title():
+#    driver.get("https://wikiroulette.co/")
+#    title_element = driver.find_element(By.CSS_SELECTOR, "#firstHeading span")
+#    title = title_element.text
+#    print(title)
+#    return title
 
-    def get_website_title(self, website):
-        try:
-            self.driver.get(website)
-            page_title = self.driver.title
-            print(page_title)
-            return page_title
-        except:
-            print('se site n\'existe pas !')
-            return ''
+def get_website_title(website):
+    try:
+        driver.get(website)
+        page_title = driver.title
+        print(page_title)
+        return page_title
+    except:
+        print('se site n\'existe pas !')
+        return ''
 
 if __name__ == '__main__':
-    driver = Sel_driver()
     driver.get_website_title("https://www.reddit.com/")
