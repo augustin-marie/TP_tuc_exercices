4a57d9f0f42f169951fe1b6b9505560e1db3280c
augustin-marie <a.marie189@laposte.net>
2023-02-17 15:45:00 +0100

    test fix

--- application/selDriver.py
+++ application/selDriver.py
@@ -1,28 +1,16 @@
-"""
-    functions selenium web driver
-"""
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 class Sel_driver:
-    """
-        Selenium handler
-    """
-    def setup_method(self, method):
+    def __init__ (self):
         self.driver = webdriver.Chrome()
-        self.vars = {}
 
-    def teardown_method(self, method):
-        self.driver.quit()
-
-    """
-    def get_roulette_title(self):
-        self.driver.get("https://wikiroulette.co/")
-        title_element = self.driver.find_element(By.CSS_SELECTOR, "#firstHeading span")
-        title = title_element.text
-        print(title)
-        return title
-    """
+    #def get_roulette_title(self):
+    #    self.driver.get("https://wikiroulette.co/")
+    #    title_element = self.driver.find_element(By.CSS_SELECTOR, "#firstHeading span")
+    #    title = title_element.text
+    #    print(title)
+    #    return title
 
     def get_website_title(self, website):
         try:
@@ -33,3 +21,7 @@ class Sel_driver:
         except:
             print('se site n\'existe pas !')
             return ''
+
+if __name__ == '__main__':
+    driver = Sel_driver()
+    driver.get_website_title("https://www.reddit.com/")
--- tests/test_sel.py
+++ tests/test_sel.py
@@ -2,12 +2,12 @@ import pytest
 from application.selDriver import Sel_driver
 
 driver = Sel_driver()
-"""
-def test_get_roulette_title():
-    result = driver.get_roulette_title()
-    assert result != ''
-"""
+
+#def test_get_roulette_title():
+#    result = driver.get_roulette_title()
+#    assert result != ''
+
 
 def test_get_website_title():
-    result = driver.get_website_title('https://www.reddit.com/')
-    assert result == 'Reddit - Dive into anything'
+    result = driver.get_website_title("https://www.reddit.com/")
+    assert result == "Reddit - Explorez sans limite"
